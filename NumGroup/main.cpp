a2c83c2804211255ee1126b06fc07a38deca9546
shines77 <gz_shines@msn.com>
2017-03-13 14:48:28 +0800

    [工程] 添加了binary生成目录; [代码] 简单的实现了 random_pick_numbers();
    Signed-off-by: shines77 <gz_shines@msn.com>

--- NumGroup/main.cpp
+++ NumGroup/main.cpp
@@ -1,3 +1,4 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <memory.h>
@@ -12,6 +13,11 @@
 #define GROUP_COUNT         10
 
 #define DISPLAY_COLUMNS     10
+
+#define noif(statement) \
+    do { \
+        statement; \
+    } while (0);
 
 typedef signed int   i32;
 typedef unsigned int u32;
@@ -92,7 +98,7 @@ public:
 
     static value_type next() {
         // Randomize number range is [0, 2^31).
-        return this_class::next(0, 2147483647);
+        return this_class::next(0, 2147483647L);
     }
 
     static i32 next_i32(i32 min_val, i32 max_val) {
@@ -126,7 +132,7 @@ public:
 
 struct NumberInfo {
     u32 value;
-    u32 index;
+//  u32 index;
 };
 
 class Answer {
@@ -137,32 +143,55 @@ private:
         u32 minimum, maximun;
     };
 
+    u32 groups_;
     u32 length_;
+    u32 total_;
+    u32 sum_;
+    u32 average_;
+    u32 remain_;
+    u32 sum_limit_;
 
 public:
     std::vector<Item> groups;
     std::vector<u32> number_list;
 
 public:
-    Answer() : length_(0) {
+    Answer() : groups_(0), length_(0), total_(0),
+               sum_(0), average_(0), remain_(0), sum_limit_(0) {
     }
-    Answer(u32 length) : length_(length) {
-        init(length, false);
+    Answer(u32 n_groups, u32 n_length)
+        : groups_(n_groups), length_(n_length), total_(0),
+          sum_(0), average_(0), remain_(0), sum_limit_(0) {
+        init(n_groups, false);
+        get_basic_info();
     }
-    Answer(u32 length, std::vector<u32> const & numbers) : length_(length) {
-        init(length, false);
-        set_numbers(numbers);
+    Answer(u32 n_groups, u32 n_length, std::vector<u32> const & numbers)
+        : groups_(n_groups), length_(n_length), total_(0),
+          sum_(0), average_(0), remain_(0), sum_limit_(0) {
+        init(n_groups, false);
+        copy_numbers(numbers);
     }
 
     ~Answer() {}
 
-    void set_numbers(std::vector<u32> const & numbers) {
+    u32 number_count() const { return total_; }
+
+    void copy_numbers(std::vector<u32> const & numbers) {
         detail::copy_container< std::vector<u32> >(number_list, numbers);
+        length_ = number_list.size();
+        get_basic_info();
     }
 
-    void resize(u32 length, bool clear = true) {
-        init(length, clear);
-        length_ = length;
+    void set_basic_info(u32 sum, u32 average, u32 remain) {
+        sum_ = sum;
+        average_ = average;
+        remain_ = remain;
+        sum_limit_ = average_ * 2 / 3;
+    }
+
+    void resize(u32 n_groups, bool clear = true) {
+        init(n_groups, clear);
+        groups_ = n_groups;
     }
 
     Item const & get_groups(int index) const {
@@ -171,32 +200,80 @@ public:
     }
 
     void copy_from(Answer const & src) {
-        if (this->groups.size() == src.groups.size()) {
-            detail::copy_container(this->groups, src.groups);
-        }
+        this->groups_ = src.groups_;
+        this->length_ = src.length_;
+
+        this->groups.clear();
+        detail::copy_container(this->groups, src.groups);
+        assert(this->groups_ == src.groups.size());
+
+        this->number_list.clear();
+        copy_numbers(src.number_list);        
+        assert(this->length_ == this->number_list.size());
+        get_basic_info();
     }
 
-    void pick_numbers(u32 group_index, i32 index1, i32 index2) {
-        assert(group_index >= 0 && group_index < groups.size());
-        if (group_index >= 0 && group_index < groups.size()) {
-            NumberInfo number_info;
-            number_info.value = number_list[index1];
-            number_info.index = index1;
-            groups[group_index].numbers.push_back(number_info);
-            number_info.value = number_list[index2];
-            number_info.index = index2;
-            groups[group_index].numbers.push_back(number_info);
+    bool move_num_to_last(i32 index, i32 remain_length) {
+        assert(remain_length > 0 && remain_length <= (i32)number_list.size());
+        assert(index >= 0 && index <= (i32)number_list.size());
+        if (remain_length > 0) {
+            if (index != remain_length - 1)
+                std::swap(number_list[index], number_list[remain_length - 1]);
         }
+        return (remain_length > 0);
     }
 
-    void pick_numbers(u32 group_index, i32 index) {
+    bool pick_numbers(u32 group_index, i32 index1, i32 index2, i32 remain_length) {
         assert(group_index >= 0 && group_index < groups.size());
+        assert(remain_length > 1 && remain_length <= (i32)number_list.size());
+        assert(index1 >= 0 && index1 < (i32)number_list.size());
+        assert(index2 >= 0 && index2 < (i32)number_list.size());
         if (group_index >= 0 && group_index < groups.size()) {
-            NumberInfo number_info;
-            number_info.value = number_list[index];
-            number_info.index = index;
-            groups[group_index].numbers.push_back(number_info);
+            if (index1 != index2 && remain_length >= 2) {                
+                u32 value1, value2;
+                value1 = number_list[index1];
+                value2 = number_list[index2];
+
+                if (groups[group_index].sum + value1 + value2 <= sum_limit_) {
+                    NumberInfo number_info;
+                    number_info.value = value1;
+                    groups[group_index].numbers.push_back(number_info);
+                    move_num_to_last(index1, remain_length);
+                    groups[group_index].sum += value1;
+                    groups[group_index].length++;
+                    total_++;
+
+                    number_info.value = value2;
+                    groups[group_index].numbers.push_back(number_info);
+                    move_num_to_last(index2, remain_length - 1);
+                    groups[group_index].sum += value2;
+                    groups[group_index].length++;
+                    total_++;
+                    return true;
+                }
+            }
         }
+        return false;
+    }
+
+    bool pick_numbers(u32 group_index, i32 index, i32 remain_length) {
+        assert(group_index >= 0 && group_index < groups.size());
+        assert(remain_length >= 1 && remain_length <= (i32)number_list.size());
+        assert(index >= 0 && index < (i32)number_list.size());
+        if (group_index >= 0 && group_index < groups.size() && remain_length >= 1) {            
+            u32 value = number_list[index];
+            if (groups[group_index].sum + value <= sum_limit_) {
+                NumberInfo number_info;
+                number_info.value = value;
+                groups[group_index].numbers.push_back(number_info);
+                move_num_to_last(index, remain_length);
+                groups[group_index].sum += value;
+                groups[group_index].length++;
+                total_++;
+                return true;
+            }
+        }
+        return false;
     }
 
     void sort_numbers(bool is_asc = true) {
@@ -224,14 +301,14 @@ public:
     }
 
 private:
-    void init(u32 length, bool clear) {
+    void init(u32 n_group, bool clear) {
         if (clear) {
             groups.clear();
         }
-        groups.resize(length);
+        groups.resize(n_group);
 
-        u32 capacity = length * 3;
-        for (size_t i = 0; i < length; ++i) {
+        u32 capacity = n_group * 3;
+        for (size_t i = 0; i < n_group; ++i) {
             if (clear) {
                 groups[i].numbers.clear();
             }
@@ -241,7 +318,18 @@ private:
             groups[i].minimum = 2147483647;
             groups[i].maximun = 0;
         }
-    }    
+    }
+
+    void get_basic_info() {
+        u32 sum = 0;
+        for (size_t i = 0; i < number_list.size(); ++i) {
+            sum += number_list[i];
+        }
+        sum_ = sum;
+        average_ = sum / groups_;
+        remain_ = sum % length_;
+        sum_limit_ = average_ * 2 / 3;
+    }
 };
 
 namespace detail {
@@ -269,7 +357,7 @@ void display_answer_detail(Answer const & answer, u32 sum, u32 length, i32 group
     double std_diff = 0.0;
     i32 n_diff = 0, n_average = (i32)sum / groups;
 
-    printf("Answer[%u] = {\n", length);
+    printf("Answer[%u] = {\n", answer.number_count());
     for (i32 g = 0; g < groups; ++g) {
         printf("    group[%2d]: { ", g + 1);
         u32 sum = 0;
@@ -304,7 +392,7 @@ class NumberGroup {
 private:
     typedef RandomGenerator<u32> RandomGen;
 
-    u32 length_, groups_;
+    u32 groups_, length_;
     u32 min_num_, max_num_;
     u32 sum_, average_, remain_;
     std::vector<u32> numbers_;
@@ -312,16 +400,16 @@ private:
     Answer answer_;
 
 public:
-    NumberGroup(u32 length, u32 groups, u32 min_num, u32 max_num)
-        : length_(length), groups_(groups), min_num_(min_num), max_num_(max_num),
+    NumberGroup(u32 groups, u32 length, u32 min_num, u32 max_num)
+        : groups_(groups), length_(length), min_num_(min_num), max_num_(max_num),
           sum_(0), average_(0), remain_(0) {
         // Generate randomize numbers.
         generate_numbers(length, min_num, max_num);
     };
     ~NumberGroup() {}
 
-    u32 length() const { return length_; }
     u32 groups() const { return groups_; }
+    u32 length() const { return length_; }
     std::vector<u32> const & numbers() const { return numbers_; }
 
     void display_numbers() {
@@ -378,6 +466,7 @@ public:
         u32 step = length_ / 2;
         u32 group_index = 0;
 
+        i32 remain_length = (i32)length_;
         i32 front, back;
         front = length_ / 2 - 1;
         if (front < 0)
@@ -385,9 +474,10 @@ public:
         back = front + 1;
         for (u32 i = 0; i < step; ++i) {
             group_index = RandomGen::next(groups_);
-            result.pick_numbers(group_index, front, back);
+            result.pick_numbers(group_index, front, back, remain_length);
             group_index++;
             group_index %= groups_;
+            remain_length -= 2;
             front--;
             back++;
             if (front < 0 || back >= (i32)length_)
@@ -395,10 +485,10 @@ public:
         }
 
         if (front < 0 && back < (i32)length_) {
-            result.pick_numbers(group_index, back);
+            result.pick_numbers(group_index, back, remain_length);
         }
         else if (front >= 0 && back >= (i32)length_) {
-            result.pick_numbers(group_index, front);
+            result.pick_numbers(group_index, front, remain_length);
         }
         
         return 1;
@@ -406,12 +496,15 @@ public:
 
     int random_pick_numbers(Answer & result) {
         i32 remain_length = (i32)length_;
-        while (remain_length >= 0) {
+        const i32 pick_nums = length_ * 2 / 3;
+        while (remain_length > ((i32)length_ - pick_nums)) {
             u32 group_index = RandomGen::next(groups_);
             i32 rand_index = RandomGen::next_i32(remain_length);
-            result.pick_numbers(group_index, rand_index);
+            if (result.pick_numbers(group_index, rand_index, remain_length)) {
+                remain_length--;
+            }
         }
-        return 0;
+        return 1;
     }
 
     int slove() {
@@ -421,9 +514,11 @@ public:
         display_sorted_numbers();
 
         answer_.resize(groups_);
-        answer_.set_numbers(sorted_list_);
+        answer_.copy_numbers(sorted_list_);
+
+        RandomSeed::init();
         
-        Answer answer(groups_, sorted_list_);
+        Answer answer(groups_, length_, sorted_list_);
         int results = random_pick_numbers(answer);
         if (results > 0) {
             answer_.copy_from(answer);
@@ -447,9 +542,9 @@ private:
 
 int main(int argc, char * argv[])
 {
-    RandomSeed::init();
+    //RandomSeed::init();
 
-    NumberGroup slover(NUMBER_COUNT, GROUP_COUNT, 5, 100);
+    NumberGroup slover(GROUP_COUNT, NUMBER_COUNT, 5, 100);
 
     slover.display_numbers();
 
